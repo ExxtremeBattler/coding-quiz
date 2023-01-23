9a1ba1ba67c2320727d5203700380f0f6750ae17
ExxtremeBattler <69109391+ExxtremeBattler@users.noreply.github.com>
2023-01-23 14:10:30 +0000

    fixed timer, main text disappears when start clicked, answer buttons appear when start clicked

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,5 +1,10 @@
-var currentTime = document.querySelector("#time").innerHTML
-var startButton = document.querySelector("#start")
+let currentTime = document.querySelector("#time").innerHTML
+let startButton = document.querySelector("#start")
+let header = document.querySelector("h1")
+let mainText = document.querySelector("#mainText")
+let button2 = document.querySelector("#button2")
+let button3 = document.querySelector("#button3")
+let button4 = document.querySelector("#button4")
 
 // Array containing questions
 
@@ -11,29 +16,36 @@ let questions = [
     "Is it necessary to end each line of code with a semicolon in Javascript?"
 ]
 
-// Create function that counts down from current time
 
-// function countingDown() {
-//     currentTime--
-//     seti
-// }
-
-
-
-
-// // Adds events that occur once the start button is clicked
-
-currentTime = 20
+// Once start is clicked, begin the countdown
 
 startButton.addEventListener("click", function () {
-    setInterval(function() {
+  currentTime = 10
+  header.classList.add("hide")
+  button2.classList.remove("hide")
+  button3.classList.remove("hide")
+  button4.classList.remove("hide")
+
+
+  // add a for loop here
+  mainText.innerHTML = " "
+// for (let index = 0; index < questions.length; index++) {
+//   mainText.innerHTML = questions[index]
+// }
+    
+setInterval(function() {
         if (currentTime > 0) {
           currentTime--;
+          document.querySelector("#time").innerHTML = currentTime
           console.log(currentTime);
         }
-      }
+      }, 1000
       )
-    }, 1000);
+    });
+
+  
+
+
 
 
 
